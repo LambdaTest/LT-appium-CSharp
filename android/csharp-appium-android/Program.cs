1817fe6b82cff4c5c04fe7686ac61a93f596a969
shashankj-lt <94521501+shashankj-lt@users.noreply.github.com>
2022-04-28 22:38:54 +0530

    First commit

---
+++ android/csharp-appium-android/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Appium.iOS;
+using System.Threading;
+
+namespace csharp_appium
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppiumOptions caps = new AppiumOptions();
+ 
+            caps.AddAdditionalCapability("LT_USERNAME", "LT_USERNAME");  //Add the LT Username
+            caps.AddAdditionalCapability("LT_ACCESS_KEY", "LT_ACCESS_KEY");  //Add the LT Access key
+
+            // Set URL of the application under test
+            caps.AddAdditionalCapability("app", "App_ID"); //Add the App ID
+
+            // Specify device and os_version
+            caps.AddAdditionalCapability("deviceName", "Galaxy S21 Ultra 5G");  //Add the Device Details
+            caps.AddAdditionalCapability("platformVersion", "11");
+            caps.AddAdditionalCapability("platformName", "Android");
+            caps.AddAdditionalCapability("isRealMobile", true);
+            caps.AddAdditionalCapability("network", true);
+
+            caps.AddAdditionalCapability("project", "CSharp Sample Android");
+            caps.AddAdditionalCapability("build", "CSharp Sample Android");
+            caps.AddAdditionalCapability("name", "CSharp Sample Android");
+
+            // Initialize the remote Webdriver using LambdaTest remote URL
+            // and desired capabilities defined above
+            IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(
+                new Uri("https://beta-hub.lambdatest.com/wd/hub"), caps);
+
+            // Test case for the sample iOS app. 
+            // If you have uploaded your app, update the test case here.
+            IOSElement color = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("color"))
+            );
+            color.Click();
+            color.Click();
+
+            IOSElement text = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("Text"))
+            );
+            text.Click();
+
+            IOSElement toast = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("toast"))
+            );
+            toast.Click();
+
+            IOSElement nf = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("notification"))
+            );
+            nf.Click();
+
+            IOSElement gl = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("geoLocation"))
+            );
+            gl.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement st = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("speedTest"))
+            );
+            st.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement browser = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Browser"))
+            );
+            browser.Click();
+
+            IOSElement inputBox = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("url"))
+            );
+            inputBox.Click();
+
+
+            driver.Quit();
+        }
+    }
+}
---
+++ ios/csharp-appium-ios/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Appium.iOS;
+using System.Threading;
+
+namespace csharp_appium_first
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppiumOptions caps = new AppiumOptions();
+
+
+            // Set your BrowserStack access credentials
+            caps.AddAdditionalCapability("LT_USERNAME", "Your_Username"); //Enter the Username here
+            caps.AddAdditionalCapability("LT_ACCESSKEY", "Your_AccessKey");  //Enter the Access key here
+
+            // Set URL of the application under test
+            caps.AddAdditionalCapability("app", "LT_App"); //Enter the App URL here.
+
+            // Specify device and os_version
+            caps.AddAdditionalCapability("deviceName", "iPhone 12"); //Change the device name here
+            caps.AddAdditionalCapability("platformVersion", "15"); 
+            caps.AddAdditionalCapability("platformName", "iOS");
+            caps.AddAdditionalCapability("isRealMobile", true);
+            caps.AddAdditionalCapability("network", true);
+
+            // Specify the platformName
+            //caps.PlatformName = "iOS";
+
+            // Set other BrowserStack capabilities
+            caps.AddAdditionalCapability("project", "First CSharp project");
+            caps.AddAdditionalCapability("build", "CSharp iOS");
+            caps.AddAdditionalCapability("name", "first_test");
+
+            // Initialize the remote Webdriver using LambdaTest remote URL
+            // and desired capabilities defined above
+            IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(
+                new Uri("https://beta-hub.lambdatest.com/wd/hub"), caps);
+
+            // Test case for the sample iOS app. 
+            // If you have uploaded your app, update the test case here.
+            IOSElement color = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("color"))
+            );
+            color.Click();
+            color.Click();
+
+            IOSElement text = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("Text"))
+            );
+            text.Click();
+
+            IOSElement toast = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("toast"))
+            );
+            toast.Click();
+
+            IOSElement nf = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("notification"))
+            );
+            nf.Click();
+
+            IOSElement gl = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("geoLocation"))
+            );
+            gl.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement st = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("speedTest"))
+            );
+            st.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement browser = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Browser"))
+            );
+            browser.Click();
+
+            IOSElement inputBox = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("url"))
+            );
+            inputBox.Click();
+
+
+            driver.Quit();
+
+        }
+    }
+}
