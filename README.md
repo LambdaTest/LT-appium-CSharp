3a0e1830f4b8c9cf9d4a6884c886618d4301f957
Shashank Jangra <94521501+shashankj-lt@users.noreply.github.com>
2022-04-29 14:00:23 +0530

    First PR for C# (#1)
    * First commit
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Removed unnecessary files.
    
    * Removed a few typos.
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Changed the name of the Repo in Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# LT_CSharp_Appium
-Sample repo to run app automation on real device on LambdaTest.
+# LT-appium-c#
+
+### Prerequisites
+
+1. Visual Studio 2019
+
+    - If not installed, download and install Visual Studio 2019 from [here](https://visualstudio.microsoft.com/vs/)
+
+## Getting Started
+
+## Steps to Run your First Test
+
+Step 1. Clone the csharp-appium Repository.
+
+```
+git clone https://github.com/LambdaTest/LT_CSharp_Appium.git
+```
+
+**1. Upoad your Android or iOS App**
+
+Upload your Android app (.apk or .aab file) or iOS app (.ipa file) to LambdaTest servers using our REST API. Here is an example cURL request :
+
+Step 4. To run Android Test.
+
+Open the iOS Project file & run the code by clicking on the 'Play' icon in Visual Studio.
+
+Step 5. To run iOS Test.
+
+Open the iOS Project file & run the code by clicking on the 'Play' icon in Visual Studio.
+
+## See the Results
+
+You can see the results of the test on Lambdatest [Automation Dashboard](https://automation.lambdatest.com/build)
+![Dashboard](https://github.com/LambdaTest/junit-selenium-sample/dashboard)
+
+## Testing Locally Hosted or Privately Hosted Projects
+
+To help you perform cross browser testing of your locally stored web pages, LambdaTest provides an SSH(Secure Shell) tunnel connection with the name Lambda Tunnel. With Lambda Tunnel, you can test your locally hosted files before you make them live over the internet. You could even perform cross browser testing from different IP addresses belonging to various geographic locations. You can also use LambdaTest Tunnel to test web-apps and websites that are permissible inside your corporate firewall.
+
+- Set tunnel value to True in test capabilities
+  > OS specific instructions to download and setup tunnel binary can be found at the following links.
+  >
+  > - [Windows](https://www.lambdatest.com/support/docs/display/TD/Local+Testing+For+Windows)
+  > - [Mac](https://www.lambdatest.com/support/docs/display/TD/Local+Testing+For+MacOS)
+  > - [Linux](https://www.lambdatest.com/support/docs/display/TD/Local+Testing+For+Linux)
+  >   After setting tunnel you can also see the active tunnel in our LambdaTest dashboard:
+  >   ![tunnel active](https://github.com/LambdaTest/Robot-Selenium-Sample/blob/master/tutorial-images/tn.PNG)
+
+## About LambdaTest
+
+[LambdaTest](https://www.lambdatest.com/) is a cloud based selenium grid infrastructure that can help you run automated cross browser compatibility tests on 2000+ different browser and operating system environments. LambdaTest supports all programming languages and frameworks that are supported with Selenium, and have easy integrations with all popular CI/CD platforms. It's a perfect solution to bring your [selenium automation testing](https://www.lambdatest.com/selenium-automation) to cloud based infrastructure that not only helps you increase your test coverage over multiple desktop and mobile browsers, but also allows you to cut down your test execution time by running tests on parallel.
---
+++ ios/csharp-appium-ios/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Appium.iOS;
+using System.Threading;
+
+namespace csharp_appium_first
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppiumOptions caps = new AppiumOptions();
+
+            caps.AddAdditionalCapability("LT_USERNAME", "Your_Username"); //Enter the Username here
+            caps.AddAdditionalCapability("LT_ACCESSKEY", "Your_AccessKey");  //Enter the Access key here
+
+            // Set URL of the application under test
+            caps.AddAdditionalCapability("app", "LT_App"); //Enter the App URL here.
+
+            // Specify device and os_version
+            caps.AddAdditionalCapability("deviceName", "iPhone 12"); //Change the device name here
+            caps.AddAdditionalCapability("platformVersion", "15"); 
+            caps.AddAdditionalCapability("platformName", "iOS");
+            caps.AddAdditionalCapability("isRealMobile", true);
+            caps.AddAdditionalCapability("network", true);
+
+            caps.AddAdditionalCapability("project", "First CSharp project");
+            caps.AddAdditionalCapability("build", "CSharp iOS");
+            caps.AddAdditionalCapability("name", "first_test");
+
+            IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(
+                new Uri("https://beta-hub.lambdatest.com/wd/hub"), caps);
+
+            IOSElement color = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("color"))
+            );
+            color.Click();
+            color.Click();
+
+            IOSElement text = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("Text"))
+            );
+            text.Click();
+
+            IOSElement toast = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("toast"))
+            );
+            toast.Click();
+
+            IOSElement nf = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("notification"))
+            );
+            nf.Click();
+
+            IOSElement gl = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("geoLocation"))
+            );
+            gl.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement st = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("speedTest"))
+            );
+            st.Click();
+
+            Thread.Sleep(5000);
+
+            driver.Navigate().Back();
+
+            IOSElement browser = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.AccessibilityId("Browser"))
+            );
+            browser.Click();
+
+            IOSElement inputBox = (IOSElement)new WebDriverWait(driver, TimeSpan.FromSeconds(30)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(MobileBy.Id("url"))
+            );
+            inputBox.Click();
+
+
+            driver.Quit();
+
+        }
+    }
+}
